82c3e6dd1cfaf77f308001aaf73aad0901331df6
samuel Ashagrre Guigsa <ashagire6@gmail.com>
2024-07-30 12:15:04 -0700

    Added my project

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// index.js
+// server.js
 // where your node app starts
 
 // init project
@@ -7,24 +7,33 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
+// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+
+// your first API endpoint... 
+app.get("/api/whoami", function (req, res) {
+  const ipAddress = req.ip;
+  const fowardedIp = req.headers["x-forwarded-for"];
+  //To get the correct ip, 'x-forwarded-for' must set when the request pass through an HTTP proxy or load balancer. For more infrmation https://www.rfc-editor.org/rfc/rfc7239#page-6
+  console.log(JSON.stringify(req.headers));
+  const acceptLanguage = req.headers['accept-language'];
+  const userAgent = req.headers['user-agent'];
+  res.send({ipaddress : ipAddress, language: acceptLanguage, software: userAgent, fowardedIp: fowardedIp});
+})
+
+
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
